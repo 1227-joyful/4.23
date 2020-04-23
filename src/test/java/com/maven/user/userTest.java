218c35fbace970df09f351eb02f68dc4010e5040
wangchao <714350524@qq.com>
2020-04-23 08:57:36 +0800

    第一次提交

---
+++ src/test/java/com/maven/user/userTest.java
@@ -0,0 +1,13 @@
+package com.maven.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class userTest {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+	}
+
+}
